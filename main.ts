21a6a6607705169ddc7812947d8be885307ab0bf
jinyalin16 <131965612+jinyalin16@users.noreply.github.com>
2023-04-27 21:16:00 +0900

    pxt.json, main.blocks, main.ts 업데이트하기

--- main.ts
+++ main.ts
@@ -1,27 +1,33 @@
-input.onGesture(Gesture.TiltRight, function () {
-    뱀.change(LedSpriteProperty.X, 1)
-})
-input.onGesture(Gesture.TiltLeft, function () {
+input.onButtonPressed(Button.A, function () {
     뱀.change(LedSpriteProperty.X, -1)
 })
-input.onGesture(Gesture.LogoDown, function () {
-    뱀.change(LedSpriteProperty.Y, -1)
-})
-input.onGesture(Gesture.LogoUp, function () {
+input.onButtonPressed(Button.AB, function () {
     뱀.change(LedSpriteProperty.Y, 1)
+})
+input.onButtonPressed(Button.B, function () {
+    뱀.change(LedSpriteProperty.X, 1)
+})
+input.onLogoEvent(TouchButtonEvent.Touched, function () {
+    뱀.change(LedSpriteProperty.Y, -1)
 })
 let 뱀: game.LedSprite = null
 뱀 = game.createSprite(2, 2)
+led.setBrightness(128)
 let 먹이 = game.createSprite(randint(0, 4), randint(0, 4))
 basic.forever(function () {
     if (뱀.isTouching(먹이)) {
+        music.playTone(262, music.beat(BeatFraction.Half))
         game.addScore(1)
         먹이.delete()
         basic.pause(100)
         먹이 = game.createSprite(randint(0, 4), randint(0, 4))
-        if (game.score() == 5) {
-            basic.showIcon(IconNames.Happy)
-            music.startMelody(music.builtInMelody(Melodies.Dadadadum), MelodyOptions.Once)
+        if (game.score() == 2) {
+            for (let index = 0; index < 3; index++) {
+                basic.showIcon(IconNames.Heart)
+                basic.pause(500)
+                basic.showIcon(IconNames.SmallHeart)
+                music.startMelody(music.builtInMelody(Melodies.Nyan), MelodyOptions.OnceInBackground)
+            }
             game.gameOver()
         }
     }
